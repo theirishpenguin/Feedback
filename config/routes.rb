15999eaa983b2f6ab0075d693ede7f2223a6689e
Declan <declan@weuseopensource.com>
2011-07-14 21:25:04 +0100

    Do it.

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do |map|
 
   mount_at = Feedback::Engine.config.mount_at
 
-  match mount_at => 'cheese/widgets#index'
+  #match mount_at => 'cheese/widgets#index'
 
-  map.resources :widgets, :only => [ :index, :show ],
-                          :controller => "cheese/widgets",
+  match mount_at => 'feedback/feedback#new'
+
+  map.resource :feedback, :only => [ :new ],
+                          :controller => "feedback/feedback",
                           :path_prefix => mount_at,
-                          :name_prefix => "cheese_"
+                          :name_prefix => "feedback_"
+
+#  map.resources :widgets, :only => [ :index, :show ],
+#                          :controller => "cheese/widgets",
+#                          :path_prefix => mount_at,
+#                          :name_prefix => "cheese_"
 
 end
